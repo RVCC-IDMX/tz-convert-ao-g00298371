55fa55f953b8dff53d97c081c9e74bc8af89723b
Your Name <you@example.com>
2023-11-08 17:26:00 -0500

    Default timezone set.

---
+++ tz.js
@@ -0,0 +1,7 @@
+const moment = require('moment-timezone');
+
+moment.tz.setDefault('America/New_York');
+
+const targetTimezone = 'Europe/Paris';
+console.log(process.argv);
+console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
