27c600d2ac3a87475c9b6bdeafe6d1a498e9b907
Your Name <you@example.com>
2023-11-12 14:22:58 -0500

    ...

--- tz.js
+++ tz.js
@@ -1,14 +1,15 @@
 const moment = require('moment-timezone');
 
 moment.tz.setDefault('America/New_York');
+const targeTimezone = 'Europe/Paris';
 
-if (process.argv.length != 3) {
+if (process.argv.length !== 3) {
   console.log('Usage: node <script-file> <timezone>');
   process.exit(1);
 } else {
   targetTimezone = process.argv[2];
 }
 
-const targetTimezone = 'Europe/Paris';
+console.log(targeTimezone);
 console.log(process.argv);
 console.log(`The time at the ${targetTimezone} timezone is ${moment().tz(targetTimezone).format()}`);
